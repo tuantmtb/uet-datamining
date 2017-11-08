4994c181a9a99e484996d9dd45facf75f548b704
SINH-PC\Admin <sinh3497@gmail.com>
2017-11-09 02:39:28 +0700

    fix tfidf

--- deep_model.py
+++ deep_model.py
@@ -55,17 +55,24 @@ def get_data(connection):
 
 
 
-def extract_data(docs):
-    contents = [doc['tokenize'] for doc in docs]
-    # print(contents[1])
-    targets= [doc['label'] for doc in docs]
-
+def extract_data(train_docs,test_docs):
+    contents = [doc['tokenize'] for doc in train_docs]
+    contents_test = [doc['tokenize'] for doc in test_docs]
+    merge_contents = contents + contents_test
+    
+    targets= [doc['label'] for doc in train_docs]
+    targets_test = [doc['label'] for doc in test_docs]
     tok = TfidfVectorizer(max_features=NUMBER_OF_TERMS,smooth_idf=True,analyzer='word',sublinear_tf=True,
                          tokenizer=lambda doc: doc.lower().split(" "),ngram_range=(2, 6))
+    tok.fit(merge_contents)
 
-    sample_texts = tok.fit_transform (contents).todense()
+    sample_texts = tok.transform (contents).todense()
     targets = np.array([1 if x == 'SPAM' else 0 for x in targets],dtype=np.int32)
-    return sample_texts,targets
+
+    sample_texts_test = tok.transform(contents_test).todense()
+    targets_test = np.array([1 if x == 'SPAM' else 0 for x in targets_test],dtype=np.int32)
+
+    return sample_texts,targets,sample_texts_test,targets_test
 
 def build_model():
     model = Sequential()
@@ -95,17 +102,16 @@ def check_model(model,x,y,epochs=4):
 
 def main():
     train_docs , test_docs = get_data(connection)
-    X_train, Y_train = extract_data(train_docs)
-    X_test, Y_test = extract_data(test_docs)
+    X_train, Y_train ,  X_test, Y_test = extract_data(train_docs,test_docs)
 
-    # model = SVC()
+    model = SVC()
 
     # train model
-    # model.fit(X_train, Y_train)
-    model = build_model()
-    his = check_model(model,X_train,Y_train,50)
-    # Y_pred = model.predict(X_test)
-    Y_pred = model.predict_classes(X_test).reshape(-1)
+    model.fit(X_train, Y_train)
+    # model = build_model()
+    # his = check_model(model,X_train,Y_train,50)
+    Y_pred = model.predict(X_test)
+    # Y_pred = model.predict_classes(X_test).reshape(-1)
     print(Y_pred)
     print(Y_test)
     print("Acc : {} ".format(accuracy_score(Y_test,Y_pred)))
